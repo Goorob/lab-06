f065837fb546515598ad33241804975c427d4c8d
Goorob <orobalswalqah@gmail.com>
2019-11-18 13:07:08 +0200

    adding Error

---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require ('express');
+
+
+const cors = require ('cors');
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use( cors() );
+
+app.get('/', (request, response) => {
+  
+  response.status(200).send('You did a great job');
+});
+
+app.get('/error', (request,response) => {
+  throw new Error('Whoops');
+});
+
+app.use('*', (request, response) =>{
+  response.status(404).send('Not Found');
+});
+
+app.use( (error, request, response) => {
+  response.status(500).send(error);
+});
+
+
+app.listen(PORT , () => console.log(`App Listening on ${PORT}`));
