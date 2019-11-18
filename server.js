7f3dbb88e4f73614631c51e0c17b4eca6bef34e4
Goorob <orobalswalqah@gmail.com>
2019-11-18 13:12:15 +0200

    working on Error

--- server.js
+++ server.js
@@ -13,21 +13,22 @@ const app = express();
 
 app.use( cors() );
 
-app.get('/', (request, response) => {
+// app.get('/', (request, response) => {
   
-  response.status(200).send('You did a great job');
-});
+//   response.status(200).send('You did a great job');
+// });
 
-app.get('/error', (request,response) => {
-  throw new Error('Whoops');
-});
+// app.get('/error', (request,response) => {
+//   throw new Error('Whoops');
+// });
 
-app.use('*', (request, response) =>{
-  response.status(404).send('Not Found');
-});
+// app.use('*', (request, response) =>{
+//   response.status(404).send('Not Found');
+// });
 
 app.use( (error, request, response) => {
   response.status(500).send(error);
+  
 });
 
 
